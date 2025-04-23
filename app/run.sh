27cf2d29c2aecedb5fe049c80af1cf5bc4c8055f
Samuski <samuski30@gmail.com>
2025-04-23 00:26:06 -0400

    Typo fix

--- app/run.sh
+++ app/run.sh
@@ -8,7 +8,7 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Running classifier..."
+echo "Exporting stats.csv..."
 python export_stats.py
-
+echo "Running classifier..."
 python classifier.py
